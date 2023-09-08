85e5283dcfdb19ea8d5ecef07b8d8f69863a95cf
lSoLoVEl <s6519410044@sau.ac.th>
2023-09-08 10:52:07 +0700

    code V1.1:Function 2

---
+++ test2.py
@@ -0,0 +1,15 @@
+#Function 2 : have parameter/have Return
+#parameter คือตัวแปรประเภทหนึ่ง ขอบเขตการใช้งานจะใช้ได้เฉพาะในฟังก์ชั่นนั้นๆ เท่านั้น
+
+def funcA( x, y ) :
+    print('konijiva')
+    z =  x + y 
+    print(f'{x} + {y} = {z}')
+    #ไม่มีคำสั่งreturn
+
+def funcB( x ) :
+    print(f"x is {x} 555...")
+
+funcA(10, 20)  #argument คือจำนวนข้อมูลที่ส่งกลับ
+funcA(5, 5)
+funcB('Sau IoT')
